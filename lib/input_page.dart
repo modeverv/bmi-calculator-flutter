0bf0e773386c357bdf0b72bf338c5268cdd785f4
seijiro <modeverv@gmail.com>
2022-01-19 20:29:21 +0900

    2022-01-19 20:29:21

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -9,11 +9,53 @@ class _InputPageState extends State<InputPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI CALCULATOR'),
-      ),
-      body: Center(
-        child: Text('Body Text'),
+        appBar: AppBar(
+          title: Text('BMI CALCULATOR'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: [
+                  ReusableColumn(c: Color(0xFF1D1E33)),
+                  ReusableColumn(c: Color(0xFF1D1E33)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  ReusableColumn(c: Color(0xFF1D1E33)),
+                ],
+              ),
+            ),
+            Expanded(
+              child: Row(
+                children: [
+                  ReusableColumn(c: Color(0xFF1D1E33)),
+                  ReusableColumn(c: Color(0xFF1D1E33)),
+                ],
+              ),
+            ),
+          ],
+        ));
+  }
+}
+
+class ReusableColumn extends StatelessWidget {
+  final Color c;
+
+  ReusableColumn({
+    @required this.c,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(15.0),
+        decoration:
+            BoxDecoration(color: c, borderRadius: BorderRadius.circular(10.0)),
       ),
     );
   }
